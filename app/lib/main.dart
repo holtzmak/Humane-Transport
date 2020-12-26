0c4727a0a68d07f8475497fcdc928b4508140749
MummenRider <clark.inocalla@hotmail.com>
2020-12-26 11:23:56 -0600

    Inital Flutter Structure

---
+++ app/lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(HumaneTransportApp());
+
+class HumaneTransportApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Text('Hello World'));
+  }
+}
