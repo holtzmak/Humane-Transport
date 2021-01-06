d9f6dd9b992c272c2fbcee6073e0dee63fe62d97
MummenRider <clark.inocalla@hotmail.com>
2021-01-06 01:06:17 -0600

    Set up pre-commit hooks. This commit also solved the issue of states not being persistent by using IndexedStack widget. Incorporating changes from local to remote

--- app/lib/navigation.dart
+++ app/lib/navigation.dart
@@ -18,8 +18,9 @@ class _NavigationBarControllerState extends State<NavigationBarController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex),
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _widgetOptions,
       ),
       bottomNavigationBar: BottomNavigationBar(
         items: [
--- app/lib/screens/in_progress/in_progress.dart
+++ app/lib/screens/in_progress/in_progress.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 
-class InProgress extends StatelessWidget {
+class InProgress extends StatefulWidget {
+  @override
+  _InProgressState createState() => _InProgressState();
+}
+
+class _InProgressState extends State<InProgress> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print('In progress');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
