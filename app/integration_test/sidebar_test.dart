5ee954fff7b223958062d5f30169fe0c994fc20f
Kelly Holtzman <holtzmak@uregina.ca>
2021-01-09 20:05:43 -0600

    Add test to open the sidebar
    Simple, dumb test for experimentation

--- app/integration_test/sidebar_test.dart
+++ app/integration_test/sidebar_test.dart
@@ -1,10 +1,24 @@
+import 'package:app/humane_transport_app.dart';
+import 'package:app/main.dart' as app;
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 
 void main() {
+  // Not in setUpAll() due to: https://github.com/flutter/flutter/issues/72063
+  // This will likely be fixed soon! Check again if your'e adding test here
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
-  testWidgets("failing test example", (WidgetTester tester) async {
-    expect(2 + 2, equals(5));
+  group('siderbar test', () {
+    testWidgets("tap on the siderbar; verify open sidebar",
+        (WidgetTester tester) async {
+      app.main();
+      await tester.pumpAndSettle();
+      // Cheap way of getting at the drawer, we should use the state when we have stateful management
+      await tester.dragFrom(
+          tester.getTopLeft(find.byType(HumaneTransportApp)), Offset(300, 0));
+      // Drags are fast but software is faster, wait
+      await tester.pumpAndSettle();
+      expect(find.text("Hello"), findsOneWidget);
+    });
   });
 }
