caf9dc5c930b8f274df370843d689132912395d1
Kelly Holtzman <holtzmak@uregina.ca>
2021-01-09 19:04:48 -0600

    Minor unit test name update

--- app/test/main_test.dart
+++ app/test/main_test.dart
@@ -2,7 +2,7 @@ import 'package:app/humane_transport_app.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  testWidgets('HumaneTransportApp says hello world',
+  testWidgets('HumaneTransportApp says this is home page',
       (WidgetTester tester) async {
     await tester.pumpWidget(HumaneTransportApp());
     final textFinder = find.text('This is Home Page');
