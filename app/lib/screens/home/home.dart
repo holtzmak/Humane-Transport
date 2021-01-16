3d4eef88435e48e27ca5a18c730d171d9c0b0684
Kelly Holtzman <holtzmak@uregina.ca>
2021-01-16 16:23:42 -0600

    Refactor PdfPreview to show pushed navigation
    Also updated navigations->navigation to be grammatically correct

--- app/lib/screens/home/home.dart
+++ app/lib/screens/home/home.dart
@@ -1,7 +1,7 @@
-import 'package:app/navigations/nav_bar/navigation.dart';
-import 'package:app/navigations/side_bar/sidebar.dart';
-import 'package:flutter/material.dart';
+import 'package:app/navigation/nav_bar/navigation.dart';
+import 'package:app/navigation/side_bar/sidebar.dart';
 import 'package:app/providers/navigation_provider.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
