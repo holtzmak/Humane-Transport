8a020b38ffdebb032c540da8b31ffbcb50ef244a
Kelly Holtzman <holtzmak@uregina.ca>
2021-01-22 19:02:32 -0600

    Added loading overlay screen

---
+++ app/lib/common/widgets/loading_overlay_screen.dart
@@ -0,0 +1,40 @@
+import 'package:app/common/style/style.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+/// A factory for a loading overlay screen. Credit https://www.greycastle.se/loading-overlay-in-flutter/
+class LoadingOverlayScreen {
+  BuildContext _context;
+
+  LoadingOverlayScreen._create(this._context);
+
+  factory LoadingOverlayScreen.of(BuildContext context) {
+    return LoadingOverlayScreen._create(context);
+  }
+
+  void hide() {
+    Navigator.of(_context, rootNavigator: true).pop();
+  }
+
+  void show() {
+    showDialog(
+        context: _context,
+        barrierDismissible: false,
+        child: WillPopScope(
+            child: _LoadingOverlayScreen(), onWillPop: () async => false));
+  }
+
+  Future<T> during<T>(Future<T> future) async {
+    show();
+    return future.whenComplete(() => hide());
+  }
+}
+
+class _LoadingOverlayScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(color: OverlayColor),
+        child: Center(child: CircularProgressIndicator()));
+  }
+}
