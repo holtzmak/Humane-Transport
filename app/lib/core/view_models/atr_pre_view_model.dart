5d23ac59e3493bfa9fd76f2f6986d80c3c8a5ca7
Kelly Holtzman <holtzmak@uregina.ca>
2021-02-03 14:12:59 -0600

    Added history test for retieve one ATR

--- app/lib/core/view_models/atr_pre_view_model.dart
+++ app/lib/core/view_models/atr_pre_view_model.dart
@@ -1,18 +1,34 @@
+import 'dart:async';
+
+import 'package:app/core/services/database/database_service.dart';
+import 'package:app/core/services/service_locator.dart';
 import 'package:app/core/view_models/base_view_model.dart';
 import 'package:app/ui/widgets/atr_preview.dart';
+import 'package:flutter/cupertino.dart';
 
 class AnimalTransportRecordPreViewModel extends BaseViewModel {
-  /*
-   TODO: #130. This class should use DatabaseService to map
-   ATRPreview from the raw records, which are then pushed through here.
-   */
+  final DatabaseService _databaseService = locator<DatabaseService>();
+  StreamSubscription<AnimalTransportRecordPreview> previewSubscription;
+
+  AnimalTransportRecordPreViewModel() {
+    previewSubscription = _databaseService
+        .getUpdatedPreviews()
+        .listen((preview) => addAll([preview]));
+  }
+
+  @mustCallSuper
+  void dispose() {
+    previewSubscription.cancel();
+    super.dispose();
+  }
+
   final List<AnimalTransportRecordPreview> _animalTransportPreviews = [];
 
   List<AnimalTransportRecordPreview> get animalTransportPreviews =>
       List.unmodifiable(_animalTransportPreviews);
 
-  void add(AnimalTransportRecordPreview animalTransportPreview) {
-    _animalTransportPreviews.add(animalTransportPreview);
+  void addAll(List<AnimalTransportRecordPreview> animalTransportPreviews) {
+    _animalTransportPreviews.addAll(animalTransportPreviews);
     notifyListeners();
   }
 
