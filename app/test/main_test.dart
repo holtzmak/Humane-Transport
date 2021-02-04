34711f8273417a63468d560d92919eb085a2aa44
Kelly Holtzman <holtzmak@uregina.ca>
2021-02-03 20:10:05 -0600

    Fix main_test
    Moved test_lib because test package didn't like it in global directory. Added beginnings of test service locator.

--- app/test/main_test.dart
+++ app/test/main_test.dart
@@ -1,18 +1,24 @@
-import 'package:app/core/services/service_locator.dart';
+import 'package:app/core/services/navigation/nav_service.dart';
+import 'package:app/core/view_models/welcome_view_model.dart';
 import 'package:app/humane_transport_app.dart';
+import 'package:app/ui/views/welcome/welcome.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
+
+import 'test_lib/test_service_locator.dart';
+
+final testLocator = GetIt.instance;
 
 void main() {
   group('Application main', () {
-    setUpAll(() {
-      // TODO: Make a test locator
-      setUpLocator();
-    });
+    tearDown(() => resetForTest(testLocator));
 
-    testWidgets('HumaneTransportApp says Welcome', (WidgetTester tester) async {
+    testWidgets('first route is welcome screen', (WidgetTester tester) async {
+      addLazySingletonForTest(testLocator, () => WelcomeViewModel());
+      addLazySingletonForTest(testLocator, () => NavigationService());
       await tester.pumpWidget(HumaneTransportApp());
-      final textFinder = find.text('Welcome User');
-      expect(textFinder, findsOneWidget);
+
+      expect(find.widgetWithText(Welcome, 'Welcome!'), findsOneWidget);
     });
   });
 }
