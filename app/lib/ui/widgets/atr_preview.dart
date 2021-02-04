65c4de4c58c74e6131ca5541461cae64979c6ebc
Kelly Holtzman <holtzmak@uregina.ca>
2021-02-04 14:04:48 -0600

    Create single history screen

--- app/lib/ui/widgets/atr_preview.dart
+++ app/lib/ui/widgets/atr_preview.dart
@@ -1,4 +1,5 @@
 import 'package:app/core/models/animal_transport_record.dart';
+import 'package:app/ui/widgets/atr_display.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -10,15 +11,17 @@ class AnimalTransportRecordPreview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Material(
-        child: ListTile(
-          leading: Icon(Icons.local_shipping),
-          title: Text(
-              'Delivery for ${atr.deliveryInfo.recInfo.receiverCompanyName}'),
-          subtitle: Text(
-              '${DateFormat("yyyy-MM-dd hh:mm").format(atr.vehicleInfo.dateAndTimeLoaded)} ${atr.vehicleInfo.animalSpeciesLoaded().join(',')}'),
-        ),
-      ),
-    );
+        child: Material(
+            child: ListTile(
+      leading: Icon(Icons.local_shipping),
+      title:
+          Text('Delivery for ${atr.deliveryInfo.recInfo.receiverCompanyName}'),
+      subtitle: Text(
+          '${DateFormat("yyyy-MM-dd hh:mm").format(atr.vehicleInfo.dateAndTimeLoaded)} ${atr.vehicleInfo.animalSpeciesLoaded().join(',')}'),
+      onTap: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+              builder: (context) => AnimalTransportRecordDisplay(atr: atr))),
+    )));
   }
 }
