36b7561f6bfcc024eb53078ed0c9ba99daeaaa76
Kelly Holtzman <holtzmak@uregina.ca>
2021-02-05 19:16:06 -0600

    Add rest of tests to ATR display test

--- app/test/ui/widgets/atr_display_test.dart
+++ app/test/ui/widgets/atr_display_test.dart
@@ -1,3 +1,4 @@
+import 'package:app/core/models/animal_transport_record.dart';
 import 'package:app/core/models/shipper_info.dart';
 import 'package:app/ui/widgets/atr_display.dart';
 import 'package:flutter/material.dart';
@@ -6,6 +7,20 @@ import 'package:flutter_test/flutter_test.dart';
 import '../../test_lib/test_animal_transport_record.dart';
 
 void main() {
+  void expectInformation(WidgetTester tester, AnimalTransportRecord testRecord,
+      String headerExpected, String informationExpected) async {
+    await tester.pumpWidget(
+        new MaterialApp(home: AnimalTransportRecordDisplay(atr: testRecord)));
+
+    /* At the time of writing, we're using ExpansionPanels.
+         Despite the expanded information not being visible initially,
+         the official tests do not check visibility programmatically
+         https://github.com/flutter/flutter/blob/master/packages/flutter/test/material/expansion_panel_test.dart
+         */
+    expect(find.widgetWithText(ListTile, headerExpected), findsOneWidget);
+    expect(find.widgetWithText(ListTile, informationExpected), findsOneWidget);
+  }
+
   group('Animal Transport Record Display', () {
     testWidgets('shows right shipping info', (WidgetTester tester) async {
       final testRecord = testAnimalTransportRecord().withShipInfo(ShipperInfo(
@@ -15,19 +30,49 @@ void main() {
           departureLocationName: "Goodman Goods",
           departureAddress: testAddress(),
           shipperContactInfo: "1306111111"));
-
-      await tester.pumpWidget(
-          new MaterialApp(home: AnimalTransportRecordDisplay(atr: testRecord)));
-
-      /* At the time of writing, we're using ExpansionPanels.
-         Despite the expanded information not being visible initially,
-         the official tests do not check visibility programmatically
-         https://github.com/flutter/flutter/blob/master/packages/flutter/test/material/expansion_panel_test.dart
-         */
-      expect(find.widgetWithText(ListTile, "Shipping Information"),
-          findsOneWidget);
-      expect(find.widgetWithText(ListTile, testRecord.shipInfo.toString()),
-          findsOneWidget);
+      expectInformation(tester, testRecord, "Shipper\'s Information",
+          testRecord.shipInfo.toString());
+    });
+    testWidgets('shows right transporter info', (WidgetTester tester) async {
+      final testRecord = testAnimalTransportRecord().withTransporterInfo(
+          testTransporterInfo(
+              companyName: "John Company",
+              driverNames: ["Jane Doe", "Jack Son"],
+              trainingType: "Standard training"));
+      expectInformation(tester, testRecord, "Transporter\'s Information",
+          testRecord.tranInfo.toString());
+    });
+    testWidgets('shows right FWR info', (WidgetTester tester) async {
+      final testRecord = testAnimalTransportRecord().withFwrInfo(
+          testFwrInfo(lastFwrDate: DateTime.parse("2021-01-01 01:01")));
+      expectInformation(tester, testRecord, "Feed, Water, and Rest Information",
+          testRecord.fwrInfo.toString());
+    });
+    testWidgets('shows right loading info', (WidgetTester tester) async {
+      final testRecord = testAnimalTransportRecord().withVehicleInfo(
+          testVehicleInfo(
+              dateAndTimeLoaded: DateTime.parse("2021-02-02 02:02")));
+      expectInformation(tester, testRecord, "Loading Vehicle Information",
+          testRecord.vehicleInfo.toString());
+    });
+    testWidgets('shows right delivery info', (WidgetTester tester) async {
+      final testRecord = testAnimalTransportRecord().withDeliveryInfo(
+          testDeliveryInfo(arrivalDate: DateTime.parse("2021-03-03 03:03")));
+      expectInformation(tester, testRecord, "Delivery Information",
+          testRecord.deliveryInfo.toString());
+    });
+    testWidgets('shows right acknowledgement info',
+        (WidgetTester tester) async {
+      // TODO: Use test/mock files here once we have a way to show acks
+      final testRecord = testAnimalTransportRecord();
+      expectInformation(tester, testRecord, "Acknowledgements",
+          testRecord.ackInfo.toString());
+    });
+    testWidgets('shows right contingency info', (WidgetTester tester) async {
+      final testRecord = testAnimalTransportRecord().withContingencyInfo(
+          testContingencyInfo(goalStatement: "To move animals safely"));
+      expectInformation(tester, testRecord, "Contingency Plan",
+          testRecord.contingencyInfo.toString());
     });
   });
 }
