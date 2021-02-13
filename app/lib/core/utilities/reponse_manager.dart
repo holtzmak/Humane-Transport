71b501904420cd351449394208c501b0ad5d1116
MummenRider <clark.inocalla@hotmail.com>
2021-02-12 22:59:00 -0600

    Updated Response type as per requested

--- app/lib/core/utilities/reponse_manager.dart
+++ app/lib/core/utilities/reponse_manager.dart
@@ -1,15 +1,16 @@
 import 'package:app/core/utilities/optional.dart';
 import 'package:flutter/material.dart';
 
-enum ResponseType { success, failure, catastrophic_failure }
+enum ResponseType { success, failure }
 
 class Response<T> {
   final ResponseType type;
   final String reason;
   final Optional<T> response;
 
-  Response.failure({@required this.type, @required this.reason})
-      : response = Optional.empty();
+  Response.failure({@required this.reason})
+      : type = ResponseType.failure,
+        response = Optional.empty();
 
   Response.success({@required this.response})
       : type = ResponseType.success,
