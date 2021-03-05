8d2198e99812651d3938c1084859edbd12a4d153
Kelly Holtzman <holtzmak@uregina.ca>
2021-03-05 16:42:40 -0600

    Clean up database interface with functions we ended up using. Delegate db work to DbService from AuthService

--- app/lib/core/services/database/database_service.dart
+++ app/lib/core/services/database/database_service.dart
@@ -1,15 +1,7 @@
 import 'dart:async';
 
-import 'package:app/core/models/acknowledgement_info.dart';
 import 'package:app/core/models/animal_transport_record.dart';
-import 'package:app/core/models/atr_identifier.dart';
-import 'package:app/core/models/contingency_plan_info.dart';
-import 'package:app/core/models/delivery_info.dart';
-import 'package:app/core/models/feed_water_rest_info.dart';
-import 'package:app/core/models/loading_vehicle_info.dart';
-import 'package:app/core/models/shipper_info.dart';
 import 'package:app/core/models/transporter.dart';
-import 'package:app/core/models/transporter_info.dart';
 import 'package:app/core/services/database/database_interface.dart';
 
 class DatabaseService {
@@ -20,43 +12,17 @@ class DatabaseService {
   Future<Transporter> getTransporter(String userId) async =>
       interface.getTransporter(userId);
 
-  Future<void> newTransporter(Transporter newUser) async =>
+  Future<void> addTransporter(Transporter newUser) async =>
       interface.setNewTransporter(newUser);
+
+  Future<void> removeTransporter(String userId) async =>
+      interface.removeTransporter(userId);
 
   Future<AnimalTransportRecord> saveNewAtr(String userId) async =>
       interface.setNewAtr(userId);
 
-  Future<void> updateAtr(AtrIdentifier atr) async =>
-      interface.updateAtrIdentifier(atr);
-
-  Future<void> updateWholeAtr(AnimalTransportRecord atr) async =>
-      interface.updateWholeAtr(atr);
-
-  Future<void> setShipperInfo(String atrId, ShipperInfo shipperInfo) async =>
-      interface.setShipperInfo(atrId, shipperInfo);
-
-  Future<void> setTransporterInfo(
-          String atrId, TransporterInfo transporterInfo) async =>
-      interface.setTransporterInfo(atrId, transporterInfo);
-
-  Future<void> setLoadingVehicleInfo(
-          String atrId, LoadingVehicleInfo vehicleInfo) async =>
-      interface.setLoadingVehicleInfo(atrId, vehicleInfo);
-
-  Future<void> setFwrInfo(
-          String atrId, FeedWaterRestInfo feedWaterRestInfo) async =>
-      interface.setFwrInfo(atrId, feedWaterRestInfo);
-
-  Future<void> setDeliveryInfo(String atrId, DeliveryInfo deliveryInfo) async =>
-      interface.setDeliveryInfo(atrId, deliveryInfo);
-
-  Future<void> setContingencyPlanInfo(
-          String atrId, ContingencyPlanInfo contingencyPlanInfo) async =>
-      interface.setContingencyPlanInfo(atrId, contingencyPlanInfo);
-
-  Future<void> setAckInfo(
-          String atrId, AcknowledgementInfo acknowledgementInfo) async =>
-      interface.setAckInfo(atrId, acknowledgementInfo);
+  Future<void> saveUpdatedAtr(AnimalTransportRecord atr) async =>
+      interface.updateAtr(atr);
 
   Future<void> removeAtr(String atrId) async => interface.removeAtr(atrId);
 
--- app/lib/core/services/database/firebase_database_interface.dart
+++ app/lib/core/services/database/firebase_database_interface.dart
@@ -1,15 +1,7 @@
 import 'dart:async';
 
-import 'package:app/core/models/acknowledgement_info.dart';
 import 'package:app/core/models/animal_transport_record.dart';
-import 'package:app/core/models/atr_identifier.dart';
-import 'package:app/core/models/contingency_plan_info.dart';
-import 'package:app/core/models/delivery_info.dart';
-import 'package:app/core/models/feed_water_rest_info.dart';
-import 'package:app/core/models/loading_vehicle_info.dart';
-import 'package:app/core/models/shipper_info.dart';
 import 'package:app/core/models/transporter.dart';
-import 'package:app/core/models/transporter_info.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 import 'database_interface.dart';
@@ -20,15 +12,38 @@ class FirebaseDatabaseInterface implements DatabaseInterface {
   FirebaseDatabaseInterface(this._firestore);
 
   @override
+  Future<void> setNewTransporter(Transporter newTransporter) async => _firestore
+      .collection('transporter')
+      .doc(newTransporter.userId)
+      .set(newTransporter.toJSON());
+
+  @override
   Future<Transporter> getTransporter(String userId) async =>
       _firestore.collection('transporter').doc(userId).get().then(
           (DocumentSnapshot snapshot) => Transporter.fromJSON(snapshot.data()));
 
   @override
-  Future<void> setNewTransporter(Transporter newTransporter) async => _firestore
-      .collection('transporter')
-      .doc(newTransporter.userId)
-      .set(newTransporter.toJSON());
+  Future<void> removeTransporter(String userId) async =>
+      _firestore.collection('transporter').doc(userId).delete();
+
+  @override
+  Future<AnimalTransportRecord> setNewAtr(String userId) async {
+    final defaultAtrAsPlaceholder = AnimalTransportRecord.defaultAtr(userId);
+    return _firestore
+        .collection('atr')
+        .add(defaultAtrAsPlaceholder.toJSON())
+        .then((docRef) => defaultAtrAsPlaceholder.withDocId(docRef.id));
+  }
+
+  @override
+  Future<void> updateAtr(AnimalTransportRecord atr) async => _firestore
+      .collection('atr')
+      .doc(atr.identifier.atrDocumentId)
+      .update(atr.toJSON());
+
+  @override
+  Future<void> removeAtr(String atrDocumentId) async =>
+      _firestore.collection('atr').doc(atrDocumentId).delete();
 
   @override
   Future<List<AnimalTransportRecord>> getCompleteRecords() async => _firestore
@@ -67,81 +82,4 @@ class FirebaseDatabaseInterface implements DatabaseInterface {
       .map((snapshot) => snapshot.docs
           .map((doc) => AnimalTransportRecord.fromJSON(doc.data(), doc.id))
           .toList());
-
-  @override
-  Future<void> setShipperInfo(String atrId, ShipperInfo shipperInfo) async =>
-      _firestore
-          .collection('atr')
-          .doc(atrId)
-          .set(shipperInfo.toJSON(), SetOptions(merge: true));
-
-  @override
-  Future<void> setTransporterInfo(
-          String atrId, TransporterInfo transporterInfo) async =>
-      _firestore
-          .collection('atr')
-          .doc(atrId)
-          .set(transporterInfo.toJSON(), SetOptions(merge: true));
-
-  @override
-  Future<AnimalTransportRecord> setNewAtr(String userId) async {
-    final defaultAtrAsPlaceholder = AnimalTransportRecord.defaultAtr();
-    return _firestore
-        .collection('atr')
-        .add(defaultAtrAsPlaceholder.toJSON())
-        .then((docRef) => defaultAtrAsPlaceholder.withDocId(docRef.id));
-  }
-
-  @override
-  Future<void> setAckInfo(
-          String atrId, AcknowledgementInfo acknowledgementInfo) async =>
-      _firestore
-          .collection('atr')
-          .doc(atrId)
-          .set(acknowledgementInfo.toJSON(), SetOptions(merge: true));
-
-  @override
-  Future<void> setContingencyPlanInfo(
-          String atrId, ContingencyPlanInfo contingencyPlanInfo) async =>
-      _firestore
-          .collection('atr')
-          .doc(atrId)
-          .set(contingencyPlanInfo.toJSON(), SetOptions(merge: true));
-
-  @override
-  Future<void> setDeliveryInfo(String atrId, DeliveryInfo deliveryInfo) async =>
-      _firestore
-          .collection('atr')
-          .doc(atrId)
-          .set(deliveryInfo.toJSON(), SetOptions(merge: true));
-
-  @override
-  Future<void> setFwrInfo(
-          String atrId, FeedWaterRestInfo feedWaterRestInfo) async =>
-      _firestore
-          .collection('atr')
-          .doc(atrId)
-          .set(feedWaterRestInfo.toJSON(), SetOptions(merge: true));
-
-  @override
-  Future<void> setLoadingVehicleInfo(
-          String atrId, LoadingVehicleInfo vehicleInfo) async =>
-      _firestore
-          .collection('atr')
-          .doc(atrId)
-          .set(vehicleInfo.toJSON(), SetOptions(merge: true));
-
-  @override
-  Future<void> updateAtrIdentifier(AtrIdentifier atr) async =>
-      _firestore.collection('atr').doc(atr.atrDocumentId).update(atr.toJSON());
-
-  @override
-  Future<void> removeAtr(String atrId) async =>
-      _firestore.collection('atr').doc(atrId).delete();
-
-  @override
-  Future<void> updateWholeAtr(AnimalTransportRecord atr) async => _firestore
-      .collection('atr')
-      .doc(atr.identifier.atrDocumentId)
-      .update(atr.toJSON());
 }
