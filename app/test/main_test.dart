b0b97130b08d5ef572b5e4acf39c969c831b05a3
Kelly Holtzman <holtzmak@uregina.ca>
2021-03-07 17:13:23 -0600

    Update main test with WelcomeViewModel changes

--- app/test/main_test.dart
+++ app/test/main_test.dart
@@ -1,20 +1,30 @@
+import 'package:app/core/services/authentication/auth_service.dart';
 import 'package:app/core/services/dialog/dialog_service.dart';
 import 'package:app/core/services/navigation/nav_service.dart';
+import 'package:app/core/utilities/optional.dart';
 import 'package:app/core/view_models/welcome_screen_view_model.dart';
 import 'package:app/humane_transport_app.dart';
 import 'package:app/test/mock/test_service_locator.dart';
 import 'package:app/ui/views/welcome/welcome_screen.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
+import 'package:mockito/mockito.dart';
+
+class MockAuthenticationService extends Mock implements AuthenticationService {}
 
 final testLocator = GetIt.instance;
 
 void main() {
+  final mockAuthService = MockAuthenticationService();
+
   group('Application main', () {
     tearDown(() async => resetForTest(testLocator));
 
     testWidgets('first route is welcome screen', (WidgetTester tester) async {
-      addLazySingletonForTest(testLocator, () => WelcomeScreenViewModel());
+      testLocator
+          .registerLazySingleton<AuthenticationService>(() => mockAuthService);
+      when(mockAuthService.currentUser).thenReturn(Optional.empty());
+      addFactoryForTest(testLocator, () => WelcomeScreenViewModel());
       addLazySingletonForTest(testLocator, () => NavigationService());
       addLazySingletonForTest(testLocator, () => DialogService());
       await tester.pumpWidget(HumaneTransportApp());
