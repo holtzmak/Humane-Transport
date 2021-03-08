55f8d6402ee25a7cc7230e31e6c04f6090e3aa66
Kelly Holtzman <holtzmak@uregina.ca>
2021-03-08 00:51:48 -0600

    Update main test with Splash screen expectation

--- app/test/main_test.dart
+++ app/test/main_test.dart
@@ -1,11 +1,14 @@
 import 'package:app/core/services/auth_service.dart';
 import 'package:app/core/services/dialog_service.dart';
 import 'package:app/core/services/nav_service.dart';
-import 'package:app/core/utilities/optional.dart';
 import 'package:app/core/view_models/welcome_screen_view_model.dart';
 import 'package:app/humane_transport_app.dart';
 import 'package:app/test/mock/test_service_locator.dart';
 import 'package:app/ui/views/welcome_screen.dart';
+import 'package:app/core/view_models/splash_screen_view_model.dart';
+import 'package:app/ui/views/splash/splash_screen.dart';
+
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mockito/mockito.dart';
@@ -20,15 +23,19 @@ void main() {
   group('Application main', () {
     tearDown(() async => resetForTest(testLocator));
 
-    testWidgets('first route is welcome screen', (WidgetTester tester) async {
-      testLocator
-          .registerLazySingleton<AuthenticationService>(() => mockAuthService);
-      when(mockAuthService.currentUser).thenReturn(Optional.empty());
-      addFactoryForTest(testLocator, () => WelcomeScreenViewModel());
+    testWidgets(
+        'first route is splash screen, then automatically the welcome screen',
+        (WidgetTester tester) async {
+      addLazySingletonForTest(testLocator, () => WelcomeScreenViewModel());
+      addLazySingletonForTest(testLocator, () => SplashScreenViewModel());
+
       addLazySingletonForTest(testLocator, () => NavigationService());
       addLazySingletonForTest(testLocator, () => DialogService());
       await tester.pumpWidget(HumaneTransportApp());
-
+      expect(find.byWidgetPredicate((Widget it) => it is SplashScreen),
+          findsOneWidget);
+      // The splash screen appears for 3 seconds only
+      await tester.pump(Duration(seconds: 3));
       expect(find.widgetWithText(WelcomeScreen, 'Welcome!'), findsOneWidget);
     });
   });
