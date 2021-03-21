f7114628cfb1abbad93f78e1475421fb3740b504
Kelly Holtzman <holtzmak@uregina.ca>
2021-03-20 21:38:31 -0600

    Add mock HTTP client for tests

---
+++ app/lib/test/helper/mock_http_client.dart
@@ -0,0 +1,114 @@
+import 'dart:io';
+
+import 'package:mockito/mockito.dart';
+
+class MockHttpClient extends Mock implements HttpClient {}
+
+class MockHttpClientRequest extends Mock implements HttpClientRequest {}
+
+class MockHttpClientResponse extends Mock implements HttpClientResponse {}
+
+class MockHttpHeaders extends Mock implements HttpHeaders {}
+
+class TestHttpOverrides extends HttpOverrides {
+  HttpClient createHttpClient(SecurityContext context) {
+    return createMockImageHttpClient(context);
+  }
+}
+
+/// Returns a mock HTTP client that responds with a transparent image to all requests.
+/// Borrowed from Flutter test https://github.com/flutter/flutter_markdown/blob/a2fa53b7a29dc58b6f26b53e23a6f5a8d283dd66/test/flutter_markdown_test.dart#L506-L548
+MockHttpClient createMockImageHttpClient(SecurityContext _) {
+  final MockHttpClient client = new MockHttpClient();
+  final MockHttpClientRequest request = new MockHttpClientRequest();
+  final MockHttpClientResponse response = new MockHttpClientResponse();
+  final MockHttpHeaders headers = new MockHttpHeaders();
+
+  when(client.getUrl(any))
+      .thenAnswer((_) => new Future<HttpClientRequest>.value(request));
+  when(request.headers).thenReturn(headers);
+  when(request.close())
+      .thenAnswer((_) => new Future<HttpClientResponse>.value(response));
+  when(response.contentLength).thenReturn(_transparentImage.length);
+  when(response.statusCode).thenReturn(HttpStatus.ok);
+  when(response.listen(any)).thenAnswer((Invocation invocation) {
+    final void Function(List<int>) onData = invocation.positionalArguments[0];
+    final void Function() onDone = invocation.namedArguments[#onDone];
+    final void Function(Object, [StackTrace]) onError =
+        invocation.namedArguments[#onError];
+    final bool cancelOnError = invocation.namedArguments[#cancelOnError];
+
+    return new Stream<List<int>>.fromIterable(<List<int>>[_transparentImage])
+        .listen(onData,
+            onDone: onDone, onError: onError, cancelOnError: cancelOnError);
+  });
+
+  return client;
+}
+
+const List<int> _transparentImage = const <int>[
+  0x89,
+  0x50,
+  0x4E,
+  0x47,
+  0x0D,
+  0x0A,
+  0x1A,
+  0x0A,
+  0x00,
+  0x00,
+  0x00,
+  0x0D,
+  0x49,
+  0x48,
+  0x44,
+  0x52,
+  0x00,
+  0x00,
+  0x00,
+  0x01,
+  0x00,
+  0x00,
+  0x00,
+  0x01,
+  0x08,
+  0x06,
+  0x00,
+  0x00,
+  0x00,
+  0x1F,
+  0x15,
+  0xC4,
+  0x89,
+  0x00,
+  0x00,
+  0x00,
+  0x0A,
+  0x49,
+  0x44,
+  0x41,
+  0x54,
+  0x78,
+  0x9C,
+  0x63,
+  0x00,
+  0x01,
+  0x00,
+  0x00,
+  0x05,
+  0x00,
+  0x01,
+  0x0D,
+  0x0A,
+  0x2D,
+  0xB4,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x49,
+  0x45,
+  0x4E,
+  0x44,
+  0xAE,
+];
