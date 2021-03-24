28910c7008ffeb498c4bd93628c11bcfb8826449
Kelly Holtzman <holtzmak@uregina.ca>
2021-03-24 17:21:39 -0600

    Update ATRPreviewCard test with shared preferences changes

--- app/test/ui/widgets/atr_preview_card_test.dart
+++ app/test/ui/widgets/atr_preview_card_test.dart
@@ -1,11 +1,26 @@
+import 'package:app/core/services/shared_preferences_service.dart';
 import 'package:app/test/test_data.dart';
 import 'package:app/ui/widgets/atr_preview_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:get_it/get_it.dart';
 import 'package:intl/intl.dart';
+import 'package:mockito/mockito.dart';
+
+class MockSharedPreferencesService extends Mock
+    implements SharedPreferencesService {}
+
+final testLocator = GetIt.instance;
 
 void main() {
+  final mockSharedPreferencesService = MockSharedPreferencesService();
+
   group('ATR Preview Card', () {
+    setUpAll(() async {
+      testLocator.registerLazySingleton<SharedPreferencesService>(
+          () => mockSharedPreferencesService);
+    });
+
     testWidgets('shows right information', (WidgetTester tester) async {
       final testDate = DateTime.parse("2021-01-01 01:01");
       final testCompany = "Test Company";
@@ -41,9 +56,26 @@ void main() {
         onTap: testCallback,
       );
       await tester.pumpWidget(new MaterialApp(home: widget));
-      await tester.tap(find.text("Transport for $testCompany"));
+      await tester.tap(find.byIcon(Icons.folder));
       await tester.pumpAndSettle();
       expect(callback, true);
+    });
+
+    testWidgets('calls set as default on tap', (WidgetTester tester) async {
+      final testCompany = "Test Company";
+      final testRecWithDelInfo = testAnimalTransportRecord().withDeliveryInfo(
+          testDeliveryInfo(
+              recInfo: testReceiverInfo(receiverCompanyName: testCompany)));
+      final widget = ATRPreviewCard(
+        atr: testRecWithDelInfo,
+        onTap: () {
+          // do nothing for test
+        },
+      );
+      await tester.pumpWidget(new MaterialApp(home: widget));
+      await tester.tap(find.text("Set as my default"));
+      await tester.pumpAndSettle();
+      verify(mockSharedPreferencesService.setAtrAsDefault(any)).called(1);
     });
   });
 }
