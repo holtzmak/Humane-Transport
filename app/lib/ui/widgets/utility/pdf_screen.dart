61de22708f4419af5463321c3e90b7f4a7d48121
SanaKhan1997 <sanafahmi26@gmail.com>
2021-03-25 00:25:11 -0500

    Added Acknowledgment Images on Pdf

--- app/lib/ui/widgets/utility/pdf_screen.dart
+++ app/lib/ui/widgets/utility/pdf_screen.dart
@@ -3,6 +3,7 @@ import 'dart:ui';
 
 import 'package:app/core/models/animal_transport_record.dart';
 import 'package:app/ui/common/style.dart';
+import 'package:network_image_to_byte/network_image_to_byte.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
@@ -40,10 +41,21 @@ class _PDFScreenState extends State<PDFScreen> {
         builder: (BuildContext context, AsyncSnapshot<File> snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.waiting:
-              return Text('Loading PDF');
+              return Container(
+                  padding: EdgeInsets.all(20),
+                  alignment: Alignment.center,
+                  color: Colors.white,
+                  child: Text('Loading PDF', style: TitleTextStyle));
             default:
               if (snapshot.hasError)
-                return Text('Could not build and save PDF: ${snapshot.error}');
+                return Container(
+                    padding: EdgeInsets.all(20),
+                    alignment: Alignment.center,
+                    color: Colors.white,
+                    child: Text(
+                      'Could not build and save PDF: ${snapshot.error}',
+                      style: TitleTextStyle,
+                    ));
               else
                 return PDFViewerScaffold(
                   appBar: AppBar(
@@ -77,9 +89,38 @@ class _PDFScreenState extends State<PDFScreen> {
         });
   }
 
-  // TODO: Give this function an object to build PDF from
   Future<File> _getBuiltAndSavedPdf(AnimalTransportRecord atr) async {
     final pdf = pw.Document();
+    pw.MemoryImage tranImage;
+    pw.MemoryImage shipperImage;
+    pw.MemoryImage receiverImage;
+    try {
+      final tranAckImage =
+          pw.MemoryImage(await networkImageToByte(atr.ackInfo.transporterAck));
+      setState(() {
+        tranImage = tranAckImage;
+      });
+    } catch (e) {
+      Text("Could not load transporter's  Acknowledgment Receipt");
+    }
+    try {
+      final shipperAckImage =
+          pw.MemoryImage(await networkImageToByte(atr.ackInfo.shipperAck));
+      setState(() {
+        shipperImage = shipperAckImage;
+      });
+    } catch (e) {
+      Text("Could not load transporter's  Acknowledgment Receipt");
+    }
+    try {
+      final receiverAckImage =
+          pw.MemoryImage(await networkImageToByte(atr.ackInfo.receiverAck));
+      setState(() {
+        receiverImage = receiverAckImage;
+      });
+    } catch (e) {
+      Text("Could not load transporter's  Acknowledgment Receipt");
+    }
     pdf.addPage(pw.MultiPage(
       pageFormat: PdfPageFormat.a4,
       margin: pw.EdgeInsets.all(45),
@@ -92,7 +133,6 @@ class _PDFScreenState extends State<PDFScreen> {
             ));
       },
       build: (pw.Context context) {
-        //TODO: Add Complete Atr
         return <pw.Widget>[
           pw.Header(
               level: 0,
@@ -139,7 +179,24 @@ class _PDFScreenState extends State<PDFScreen> {
               level: 1,
               child: pw.Text("Acknowledgements",
                   style: pw.TextStyle(fontSize: 18))),
-          //TODO: Add Ack Images
+          pw.Text("Transporter's Acknowledgment"),
+          pw.SizedBox(
+            height: 250,
+            child: pw.Image(tranImage),
+          ),
+          pw.Padding(padding: pw.EdgeInsets.all(20)),
+          pw.Text("Shipper's Acknowledgment"),
+          pw.SizedBox(
+            height: 250,
+            child: pw.Image(shipperImage),
+          ),
+          pw.Padding(padding: pw.EdgeInsets.all(20)),
+          pw.Text("Receiver's Acknowledgment"),
+          pw.SizedBox(
+            height: 250,
+            child: pw.Image(receiverImage),
+          ),
+          pw.Padding(padding: pw.EdgeInsets.all(25)),
           pw.Header(
               level: 1,
               child: pw.Text("Contingency Plan",
@@ -151,10 +208,9 @@ class _PDFScreenState extends State<PDFScreen> {
       },
     ));
 
-    // TODO: Change the filename to something useful
-    return Future.wait([getTemporaryDirectory(), pdf.save()]).then(
-        (List values) =>
-            File("${values[0].path}/${atr.identifier.atrDocumentId}.pdf")
-                .writeAsBytes(values[1]));
+    return Future.wait(
+        [getTemporaryDirectory(), pdf.save()]).then((List values) => File(
+            "${values[0].path}/${atr.deliveryInfo.recInfo.destinationLocationName}${atr.deliveryInfo.arrivalDateAndTime}.pdf")
+        .writeAsBytes(values[1]));
   }
 }
