fb7ab137c369aafb2a4bc11530f4afb1b8a90241
mansiPaTTeL <mansi811@icloud.com>
2021-03-30 02:33:49 -0600

    cleaned up image-picker screen

--- app/lib/ui/widgets/utility/image_screen.dart
+++ app/lib/ui/widgets/utility/image_screen.dart
@@ -1,7 +1,4 @@
-import 'dart:async';
 import 'dart:io';
-
-import 'package:app/ui/common/style.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -16,62 +13,10 @@ class ImageScreen extends StatefulWidget {
 }
 
 class _ImageScreenState extends State<ImageScreen> {
-  PickedFile _imageFile;
-  dynamic _pickImageError;
   File _image;
-  final ImagePicker _picker = ImagePicker();
   final TextEditingController maxWidthController = TextEditingController();
   final TextEditingController maxHeightController = TextEditingController();
   final TextEditingController qualityController = TextEditingController();
-
-  void _onImageButtonPressed(ImageSource source, {BuildContext context}) async {
-    try {
-      final pickedFile = await _picker.getImage(
-        source: source,
-      );
-      setState(() {
-        _imageFile = pickedFile;
-      });
-    } catch (e) {
-      setState(() {
-        _pickImageError = e;
-      });
-    }
-  }
-
-  Widget _previewImage() {
-    if (_imageFile != null) {
-      if (kIsWeb) {
-        return Image.network(_imageFile.path);
-      } else {
-        return Semantics(
-            child: Image.file(File(_imageFile.path)),
-            label: 'image_picker_example_picked_image');
-      }
-    } else if (_pickImageError != null) {
-      return Text(
-        'Pick image error: $_pickImageError',
-        textAlign: TextAlign.center,
-      );
-    } else {
-      return const Text(
-        'You have not yet picked an image.',
-        textAlign: TextAlign.center,
-      );
-    }
-  }
-
-  Future<void> retrieveLostData() async {
-    final LostData response = await _picker.getLostData();
-    if (response.isEmpty) {
-      return;
-    }
-    if (response.file != null) {
-      setState(() {
-        _imageFile = response.file;
-      });
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -85,90 +30,38 @@ class _ImageScreenState extends State<ImageScreen> {
         ),
       ),
       body: Center(
-        child: FutureBuilder<void>(
-          future: retrieveLostData(),
-          builder: (BuildContext context, AsyncSnapshot<void> snapshot) {
-            switch (snapshot.connectionState) {
-              case ConnectionState.none:
-              case ConnectionState.waiting:
-                return const Text(
-                  'You have not yet picked an image.',
-                  textAlign: TextAlign.center,
-                );
-              case ConnectionState.done:
-                return _previewImage();
-              default:
-                if (snapshot.hasError) {
-                  return Text(
-                    'Pick image: ${snapshot.error}}',
-                    textAlign: TextAlign.center,
-                  );
-                } else {
-                  return const Text(
-                    'You have not yet picked an image.',
-                    textAlign: TextAlign.center,
-                  );
-                }
-            }
-          },
-        ),
-      ),
-      floatingActionButton: Column(
-        mainAxisAlignment: MainAxisAlignment.end,
-        children: <Widget>[
-          GestureDetector(
-            onTap: () {
-              _showPicker(context);
-            },
-            child: CircleAvatar(
-              radius: 55,
-              backgroundColor: NavyBlue,
-              child: _image != null
-                  ? ClipRRect(
-                      borderRadius: BorderRadius.circular(50),
-                      child: Image.file(
-                        _image,
-                        width: 100,
-                        height: 100,
-                        fit: BoxFit.fitHeight,
-                      ),
-                    )
-                  : Container(
-                      decoration: BoxDecoration(
+        child: Column(
+          children: <Widget>[
+            Padding(padding: EdgeInsets.all(30.0)),
+            Text("Click below to Choose an Image",
+                style: TextStyle(fontSize: 22), textAlign: TextAlign.center),
+            Padding(padding: EdgeInsets.all(30.0)),
+            GestureDetector(
+                onTap: () {
+                  _showPicker(context);
+                },
+                child: _image != null
+                    ? ClipRRect(
+                        child: Image.file(
+                          _image,
+                          width: 500,
+                          height: 500,
+                        ),
+                      )
+                    : Container(
+                        decoration: BoxDecoration(
                           color: Colors.grey[200],
-                          borderRadius: BorderRadius.circular(50)),
-                      width: 100,
-                      height: 100,
-                      child: Icon(
-                        Icons.camera_alt,
-                        color: Colors.grey[800],
-                      ),
-                    ),
-            ),
-          ),
-          Semantics(
-            label: 'image_picker_example_from_gallery',
-            child: FloatingActionButton(
-              onPressed: () {
-                _onImageButtonPressed(ImageSource.gallery, context: context);
-              },
-              heroTag: 'image0',
-              tooltip: 'Pick Image from gallery',
-              child: const Icon(Icons.photo_library),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 16.0),
-            child: FloatingActionButton(
-              onPressed: () {
-                _onImageButtonPressed(ImageSource.camera, context: context);
-              },
-              heroTag: 'image1',
-              tooltip: 'Take a Photo',
-              child: const Icon(Icons.camera_alt),
-            ),
-          ),
-        ],
+                        ),
+                        width: 500,
+                        height: 500,
+                        child: Icon(
+                          Icons.camera_alt,
+                          color: Colors.grey[500],
+                          size: 70,
+                        ),
+                      )),
+          ],
+        ),
       ),
     );
   }
